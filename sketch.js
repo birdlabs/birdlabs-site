c3737dd2aae4413cf2b4174749d378279195c5cd
birdlabs <73446431+birdlabs@users.noreply.github.com>
2021-04-30 12:27:31 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,16 +4,17 @@ var sliderRate;
 var sliderPan;
 var button;
 var filtButton;
-let filter, filterFreq, filterWidth;
+let filter, filterFreq, filterWidth, fft;
 let isConnected = true;
 
 function setup() {
- createCanvas(200,20);
+ createCanvas(300,200);
  song = loadSound("hexagen2612samp.mp3");
  sliderVol = createSlider(0, 1, 0.5, 0.01);
  sliderRate = createSlider(0, 10, 1, 0.01);
  sliderPan = createSlider(-1, 1, 0, 0.01);
  filter = new p5.BandPass();
+ fft = new p5.FFT();
  button = createButton("play");
  filtButton = createButton("filter on");
  button.mousePressed(togglePlaying);
@@ -54,4 +55,11 @@ function draw() {
  filterFreq = map(mouseX, 0, width, 10, 22050);
  filterWidth = map(mouseY, 0, height, 0, 90);
  filter.set(filterFreq, filterWidth);
+ let spectrum = fft.analyze();
+ noStroke();
+ for (let i = 0; i < spectrum.length; i++) {
+   let x = map(i, 0, spectrum.length, 0, width);
+   let h = -height + map(spectrum[i], 0, 255, height, 0);
+   rect(x, height, width / spectrum.length, h);
+  }
 }
