92beeca1e4ed27739b755285ae5b9d9e8cc028a2
birdlabs <73446431+birdlabs@users.noreply.github.com>
2021-04-30 12:04:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,15 +3,21 @@ var sliderVol;
 var sliderRate;
 var sliderPan;
 var button;
+var filtButton;
+let filter, filterFreq, filterWidth;
+let isConnected = true;
 
 function setup() {
  createCanvas(200,20);
- song = loadSound("hexagen2612samp.mp3"); 
+ song = loadSound("hexagen2612samp.mp3");
  sliderVol = createSlider(0, 1, 0.5, 0.01);
  sliderRate = createSlider(0, 10, 1, 0.01);
  sliderPan = createSlider(-1, 1, 0, 0.01);
+ filter = new p5.BandPass();
  button = createButton("play");
+ filtButton = createButton("filter on");
  button.mousePressed(togglePlaying);
+ filtButton.mousePressed(filterStuff);
 }
 
 function togglePlaying() {
@@ -26,9 +32,26 @@ function togglePlaying() {
   }
 }
 
+function filterStuff() {
+ if (isConnected) {
+  song.disconnect();
+  filter.process(song,mouseX,mouseY);
+  isConnected = false;
+  filtButton.html("filter off");
+ }
+ else {
+  song.connect();
+  isConnected = true;
+  filtButton.html("filter on");
+ }
+}
+
 function draw() {
  //background(0);
  song.setVolume(sliderVol.value());
  song.rate(sliderRate.value());
  song.pan(sliderPan.value());
+ //filterFreq = map(mouseX, 0, width, 10, 22050);
+ //filterWidth = map(mouseY, 0, height, 0, 90);
+ //filter.set(filterFreq, filterWidth);
 }
