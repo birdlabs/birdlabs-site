b4973b5b47c7a2bc2cb57dc4f242f4c873198650
birdlabs <73446431+birdlabs@users.noreply.github.com>
2021-04-30 13:06:42 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,8 @@
 var song;
-var sliderVol;
-var sliderRate;
-var sliderPan;
-var button;
-var filtButton;
-let filter, filterFreq, filterWidth, fft;
-let isConnected = true;
+var sliderVol, sliderRate, sliderPan;
+var button, bpFiltButton, lpFiltButton;
+let bpFilter, lpFilter, filterFreq, filterWidth, filterRes, fft;
+let isConnected = true, bpOn = false, lpOn = false;
 
 function setup() {
  createCanvas(710,256);
@@ -13,12 +10,15 @@ function setup() {
  sliderVol = createSlider(0, 1, 0.5, 0.01);
  sliderRate = createSlider(0, 10, 1, 0.01);
  sliderPan = createSlider(-1, 1, 0, 0.01);
- filter = new p5.BandPass();
+ bpFilter = new p5.BandPass();
+ lpFilter = new p5.LowPass();
  fft = new p5.FFT();
  button = createButton("play");
- filtButton = createButton("filter on");
+ bpFiltButton = createButton("BPF on");
+ lpFiltButton = createButton("LPF on");
  button.mousePressed(togglePlaying);
- filtButton.mousePressed(filterStuff);
+ bpFiltButton.mousePressed(bpFilterStuff);
+ lpFiltButton.mousePressed(lpFilterStuff);
 }
 
 function togglePlaying() {
@@ -33,18 +33,44 @@ function togglePlaying() {
   }
 }
 
-function filterStuff() {
- if (isConnected) {
+function bpFilterStuff() {
+ if (isConnected || lpOn) {
   song.disconnect();
-  filter.process(song);
+  bpFilter.process(song);
   isConnected = false;
-  filtButton.html("filter off");
+  bpOn = true;
+  bpFiltButton.html("BPF off");
+  if (lpOn) {
+   lpFiltButton.html("LPF on");
+   lpOn = false;
+  }
  }
  else {
   song.connect();
   isConnected = true;
-  filtButton.html("filter on");
+  bpOn = false;
+  bpFiltButton.html("BPF on");
  }
+}
+
+function lpFilterStuff() {
+ if (isConnected || bpOn) {
+  song.disconnect();
+  lpFilter.process(song);
+  isConnected = false;
+  lpOn = true;
+  lpFiltButton.html("LPF off");
+  if (bpOn) {
+   bpFiltButton.html("BPF on");
+   bpOn = false;
+  } 
+ }
+ else {
+  song.connect()
+  isConnected = true;
+  lpOn = false;
+  lpFiltButton.html("LPF on");
+ } 
 }
 
 function draw() {
@@ -53,8 +79,10 @@ function draw() {
  song.rate(sliderRate.value());
  song.pan(sliderPan.value());
  filterFreq = map(mouseX, 0, width, 10, 22050);
- filterWidth = map(mouseY, 0, height, 0, 90);
- filter.set(filterFreq, filterWidth);
+ filterWidth = map(mouseY, 0, height, 20, 90);
+ filterRes = map(mouseY, 0, height, 15, 5);
+ bpFilter.set(filterFreq, filterWidth);
+ lpFilter.set(filterFreq, filterRes);
  let spectrum = fft.analyze();
  noStroke();
  for (let i = 0; i < spectrum.length; i++) {
